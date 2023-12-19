a9fc2da4ea15625dc82cbcd689cb2ff3f7fec5a2
Lilien.86 <lilienauger.86@gmail.com>
2023-12-19 14:13:06 +0100

    resize split 100*100

--- Pixelarium - GrassLands/Sprites/Tileset/redim.sh
+++ Pixelarium - GrassLands/Sprites/Tileset/redim.sh
@@ -6,14 +6,21 @@ if ! command -v convert &>/dev/null; then
   exit 1
 fi
 
-# Nom du fichier d'entrée
-fichier_entree="void.xpm"
+# Crée un dossier "modif" s'il n'existe pas déjà
+mkdir -p modif
 
-# Nom du fichier de sortie
-fichier_sortie="void_r.xpm"
+# Parcours tous les fichiers XPM du dossier actuel
+for fichier_entree in *.xpm; do
+  if [ -f "$fichier_entree" ]; then
+    # Crée le nom de fichier de sortie dans le dossier "modif"
+    fichier_sortie="modif/${fichier_entree%.*}_100x100.xpm"
 
-# Redimensionne l'image avec ImageMagick
-convert "$fichier_entree" -resize 100x100\> "$fichier_sortie"
+    # Redimensionne l'image à exactement 100x100 pixels avec ImageMagick
+    convert "$fichier_entree" -resize 100x100! "$fichier_sortie"
 
-echo "L'image a été redimensionnée avec succès et enregistrée sous le nom '$fichier_sortie'."
+    echo "L'image '$fichier_entree' a été redimensionnée avec succès à exactement 100x100 pixels et enregistrée dans le dossier 'modif' sous le nom '$fichier_sortie'."
+  fi
+done
+
+echo "Toutes les images XPM ont été redimensionnées avec succès à exactement 100x100 pixels et enregistrées dans le dossier 'modif'."
 
